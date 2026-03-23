3387cfb9fae0806d8c7a13f90c7fab11951bf920
eriick-monteiro <erick.m.anjos@gmail.com>
2026-03-23 12:43:37 -0300

    :hammer: fix: Finalizando Solução Exercício 20

--- PythonPOO/ex004/desafio020.py
+++ PythonPOO/ex004/desafio020.py
@@ -13,18 +13,32 @@ class Gamer:
 
     def add_favoritos(self, jogo: str):
         self.jogos_favoritos.append(jogo)
+        self.jogos_favoritos = sorted(self.jogos_favoritos, key=str.lower)
         # print(f"Jogo [green]{jogo}[/] adicionado à lista de jogos favoritos de [blue]{self.nick}[/]")
 
     def ficha(self):
-        self.jogos_favoritos.sort()
-        texto = f"Nome real: [blue]{self.nome}[/]\nJogos Favoritos:"
-        ficha = Panel(texto, title=f"Jogador <{self.nick}>", width=40)
-        print(ficha)
+        jogos = "\n:video_game: ".join(self.jogos_favoritos)
+
+        conteudo = f"Nome real: [black on blue]{self.nome}[/]"
+        conteudo += "\nJogos Favoritos:"
+
+        for num, game in enumerate(self.jogos_favoritos):
+            conteudo += f"\n:video_game: [blue]{game}[/]"
+
+        painel = Panel(conteudo, title=f"Jogador <{self.nick}>", width=40)
+
+        print(painel)
 
 
-j1 = Gamer("Fabrício da Silva", "detonador2025")
+j1 = Gamer("Fabrício da Silva", "detonator2025")
 j1.add_favoritos("Mario Bros")
 j1.add_favoritos("Sonic")
 j1.add_favoritos("God of War")
-j1.add_favoritos("Mario Fortnite")
+j1.add_favoritos("Fortnite")
 j1.ficha()
+
+j2 = Gamer("Stella", "Estr3ll4")
+j2.add_favoritos("Valorant")
+j2.add_favoritos("Stardew Valley")
+j2.add_favoritos("Battlefield 5")
+j2.ficha()
